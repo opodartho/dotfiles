a48a845c195c9b821cb3b6e8ff114a2413534552
Zahidul Haque <iisowrovii@gmail.com>
2016-06-25 02:47:42 +0600

    map CAPS_LOCK key as CTRL using xmodmap

--- recipes/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo sed -i 's|http://.*.archive.ubuntu.com/ubuntu|http://archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y dist-upgrade
-sudo apt -y autoremove
