cac89a1e9eed33ccd199503e908f5c24fe26c93b
Zahidul Haque <iisowrovii@gmail.com>
2016-06-25 02:53:13 +0600

    Install rbenv and ruby.

---
+++ recipes/rbenv.sh
@@ -0,0 +1,21 @@
+# rbenv
+# $1: ruby version
+
+if [ -d ~/.rbenv ]; then
+  echo 'rbenv already installed, skipping.'
+else
+  sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+  git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export RBENV_ROOT=~/.rbenv' >> ~/.zshrc
+  echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> ~/.zshrc
+  echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+  source ~/.zshrc
+  mkdir -p ~/.rbenv/plugins
+  cd ~/.rbenv/plugins
+  git clone git://github.com/rbenv/ruby-build.git
+  echo "Compiling Ruby. Grab some hot chocolate, this will take a while..."
+  rbenv install 2.3.1
+  rbenv global 2.3.1
+  gem install bundler
+  rbenv rehash
+fi
