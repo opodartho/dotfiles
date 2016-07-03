79cfdc0e9dfc9e26eef7d5151fdcb2faf75c7465
Zahidul Haque <iisowrovii@gmail.com>
2016-07-03 18:36:07 +0600

    Add powerline font

---
+++ recipes/font-patch.sh
@@ -0,0 +1,4 @@
+if [ ! -e ~/.local/share/fonts ]; then
+  mkdir -p ~/.local/share/fonts
+  cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+fi
