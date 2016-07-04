38c7a006888b1145a494946e9c0c390e688277d2
Zahidul Haque <iisowrovii@gmail.com>
2016-07-04 22:06:43 +0600

    Update postgres install script

--- recipes/postgres.sh
+++ recipes/postgres.sh
@@ -3,19 +3,17 @@
 if package.installed "postgresql"; then
 	echo "postgresql already installed, skipping."
 else
-	sudo -i
-	echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" >> /etc/apt/sources.list.d/postgresql-pgdgp-xenial.list
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-	apt update
-	apt -y install postgresql libpq-dev
+	sudo cp files/postgresql-pgdgp-xenial.list /etc/apt/sources.list.d/postgresql-pgdgp-xenial.list
+	sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+	sudo apt update
+	sudo apt -y install postgresql libpq-dev
 
 	#enable password authentication
-	mv files/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
-	chown postgres:postgres /etc/postgresql/9.5/main/pg_hba.conf
-	chmod 640 /etc/postgresql/9.5/main/pg_hba.conf
-	service postgresql restart
+	sudo cp files/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
+	sudo chown postgres:postgres /etc/postgresql/9.5/main/pg_hba.conf
+	sudo chmod 640 /etc/postgresql/9.5/main/pg_hba.conf
+	sudo service postgresql restart
 
-	exit
 	#create user and database
 	sudo -u postgres psql -c "create user sowrov with password 'password';"
 	sudo -u postgres psql -c "ALTER USER sowrov CREATEDB;"
