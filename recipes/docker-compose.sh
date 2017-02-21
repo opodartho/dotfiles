5c092d808f1c992f92ff531176bc5b33f2dc0fe5
indrajit <eendroroy@gmail.com>
2017-02-22 04:48:42 +0600

    determine ltest docker-compose version

--- recipes/docker-compose.sh
+++ recipes/docker-compose.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
 if which docker-compose > /dev/null; then
-    echo "docker-compose already installed. Skipping ..."
+  echo "########## docker-compose already installed. Skipping ..."
 else
-    curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
-    sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+  last_release=`\curl -i https://api.github.com/repos/docker/compose/releases | grep "\"tag_name\"" | tr "\"" " " | awk '{print $3}' | head -1`
+  echo "########## Installing docker-compose ..."
+  \curl -L https://github.com/docker/compose/releases/download/${last_release}/docker-compose-`uname -s`-`uname -m` > docker-compose
+  sudo mv docker-compose /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 fi
