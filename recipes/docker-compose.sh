5e5f271d4ab373ae83c73dba6b21152d57af4c78
indrajit <eendroroy@gmail.com>
2017-02-22 04:59:52 +0600

    check if latest docker-compose version is installed

--- recipes/docker-compose.sh
+++ recipes/docker-compose.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+should_install=1
+last_release=`\curl -i https://api.github.com/repos/docker/compose/releases | grep "\"tag_name\"" | tr "\"" " " | awk '{print $3}' | head -1`
 if which docker-compose > /dev/null; then
-  echo "########## docker-compose already installed. Skipping ..."
+  installed_version=`docker-compose -v | awk '{print $3}' | tr -d ","`
+  if [[ "$installed_version" = "$last_release" ]]; then
+      should_install=0
+  fi
+fi
+
+if [[ "$should_install" = "0" ]]; then
+  echo "########## latest version of docker-compose already installed. Skipping ..."
 else
-  last_release=`\curl -i https://api.github.com/repos/docker/compose/releases | grep "\"tag_name\"" | tr "\"" " " | awk '{print $3}' | head -1`
   echo "########## Installing docker-compose ..."
   \curl -L https://github.com/docker/compose/releases/download/${last_release}/docker-compose-`uname -s`-`uname -m` > docker-compose
   sudo mv docker-compose /usr/local/bin/docker-compose
